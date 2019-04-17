50c621adc3f72f4bf3d68a5d073180864593f7e8
efrain <efarinva98@gmail.com>
2019-04-17 19:38:02 -0400

    doing article first still have a ways to go

--- Article/Article.js
+++ Article/Article.js
@@ -2,28 +2,31 @@
 
 class Article {
   constructor(domElement) {
+    console.log(domElement);
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector(".article");
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.uptext = this.domElement.querySelectorAll(".expandButton")
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+   this.domElement.addEventListener("click",() => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    console.log("click");
   }
 }
 
-/* START HERE: 
+//START HERE: 
+//- Select all classes named ".article" and assign that value to the articles variable.  
+let articles = document.querySelectorAll(".article");
 
-- Select all classes named ".article" and assign that value to the articles variable.  
+//- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
+articles.forEach((section)=>{
+  const allArts = new Article(section);
+  console.log(section,"lets see");
+});
 
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
-
-*/
-
-let articles;
+//let articles;
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,11 +1,83 @@
 
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
+
 }
 
-
 // Start Here: Create a reference to the ".menu" class
-const menu;
+//const Menu  = 
+// class Menu {
+//     constructor(obj){
+//       this.menu = obj;
+//     }//att's
+  
+// }//methods
 // create a reference to the ".menu-button" class
-const menuButton;
+//const MenuButton ;
+class MenuButton{
+    constructor(obj){
+      this.menu = obj;
+      this.menuOpen = this.menu.querySelector(".menu-button");
+      //this.menu.addEventListener("click", this.menu.bind(this))
+      this.menuOpen.addEventListener("click",() => this.openMenu());
+    }//att's
+  openMenu(){
+    console.log("menu");
+    //this.menuOpen = this.
+  }
+}//methods
+
+const menuBut = new MenuButton({
+
+})
+
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+
+
+
+// class Panel {
+//   constructor(panel) {
+//     this.panel = panel;
+//     this.panelButtons = this.panel.querySelector(".panel-buttons");
+//     this.panelBtnOpen = this.panel.querySelector(".panel-btn-open");
+//     this.panelBtnClose = this.panel.querySelector(".panel-btn-close");
+//     this.panelContent = this.panel.querySelector(".panel-content")
+    
+//     // old binding of the this
+//     // this.panelButtons.addEventListener("click", this.togglePanel.bind(this))
+    
+//     // new: Using Arrow functions
+//     this.panelButtons.addEventListener("click", this.togglePanel)
+//   }
+//   // methods
+//   togglePanel = () => {
+//     // debugger;
+//     this.panelBtnOpen.classList.toggle("hide-btn");
+//     this.panelBtnClose.classList.toggle("hide-btn");
+//     this.panelContent.classList.toggle("toggle-on")
+//   }
+// }
+
+
+// let panels = document.querySelectorAll(".panel");
+// // console.log(panels)
+
+// // panels.forEach(panel => {
+// //   new Panel(panel);
+// // })
+
+// // Converting to an array
+
+// // impplicit vs explicit
+// // map, filter, reduce DOES NOT APPLY to forEach
+// panels = Array.from(panels).map(panel => (
+//   new Panel(panel)
+// ));
+
+// // const buttonClose = document.querySelector(".secondary .accordion .panel-btn-close")
+
+// // buttonClose.classList.remove("hide-btn")
+
+// // console.log(buttonClose.classList)
