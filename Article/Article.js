604c27ca785689da7cdfe74441e6a9b87fd0609e
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-04-24 14:35:43 -0700

    Completed Part 1 + Part 2

--- Article/Article.js
+++ Article/Article.js
@@ -1,29 +1,27 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
+/* START HERE : */
+
 class Article {
   constructor(domElement) {
+    this.domElement = domElement; // ".article"
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton.innerText = 'expand';
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', () => { this.expandArticle() }); 
+    // Set a click handler on the expandButton reference, calling the expandArticle method.   
   }
-
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+  
+  expandArticle() { // Using our reference to the domElement, toggle a class to expand or hide the article.
+    this.domElement.classList.toggle('article-open');
   }
 }
 
-/* START HERE: 
-
-- Select all classes named ".article" and assign that value to the articles variable.  
-
-- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
-
-*/
-
-let articles;
+let articles = document.querySelectorAll('.article').forEach( article => {
+  new Article(article);
+    // Select all classes named ".article" and assign that value to the articles variable. 
+    // Now chain .forEach() on to the articles variable to iterate over the articles NodeList.
+    // Create a new instance of Article by passing in each article as a parameter to the Article class.
+});
